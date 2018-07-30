37ca7ed83cabf4a281207f19fd71fd74dd8b78cd
DragonBoom <1312449403@qq.com>
2018-07-30 09:39:46 +0800

    2018.07.30

---
+++ src/main/java/indi/oracle/java/util/StreamTest.java
@@ -0,0 +1,16 @@
+package indi.oracle.java.util;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+public class StreamTest {
+
+    @Test
+    void start() {
+        LinkedList<Object> l = new LinkedList<>();
+        l.stream();
+        Stream stream;
+    }
+}
