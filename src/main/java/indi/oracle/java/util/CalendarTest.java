b497159e0e9eb2ee574298e1e07dce0440d15ecb
DragonBoom <1312449403@qq.com>
2018-08-01 00:26:28 +0800

    java.util.Calendar

---
+++ src/main/java/indi/oracle/java/util/CalendarTest.java
@@ -0,0 +1,20 @@
+package indi.oracle.java.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+public class CalendarTest {
+
+    @Test
+    void go() {
+        Date date = new Date(System.currentTimeMillis());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH));// 从0开始!!
+        System.out.println(calendar.get(Calendar.DATE));
+
+    }
+}
