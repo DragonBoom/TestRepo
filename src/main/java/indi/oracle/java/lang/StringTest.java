09716e8407ba18a60492f750ec42929c7263a4e2
DragonBoom <1312449403@qq.com>
2018-09-28 18:49:42 +0800

    2018.09.28
    aa

--- src/main/java/indi/oracle/java/lang/StringTest.java
+++ src/main/java/indi/oracle/java/lang/StringTest.java
@@ -4,12 +4,30 @@ import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
-    @Test
+    // @Test
     void splitTest() {
         String s = new String("www|fff|www");
         String[] afterSplit = s.split("\\|");
         for (String str : afterSplit) {
             System.out.println(str);
         }
+    }
+    
+    // @Test
+    void formatTest() {
+        String s = new String("wwwww\nwwww");
+        System.out.println(s);
+        System.out.println(s.replace("\n", ""));
+    }
+    
+    /**
+     * 换行测试
+     */
+    @Test
+    void newLineTest() {
+        String x = "a\nf";
+        
+        System.out.println(x.indexOf("\n"));
+        System.out.println(x);
     }
 }
--- src/main/java/indi/oracle/java/util/concurrent/ForkJoinPoolTest.java
+++ src/main/java/indi/oracle/java/util/concurrent/ForkJoinPoolTest.java
@@ -1,5 +1,25 @@
 package indi.oracle.java.util.concurrent;
 
-public class ForkJoinPoolTest {
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * 使用ForkJoinPool能够使用数量有限的线程来完成非常多的具有父子关系的任务，比如使用4个线程来完成超过200万个任务。<br>
+ * 但是，使用ThreadPoolExecutor时，是不可能完成的，因为ThreadPoolExecutor中的Thread无法选择优先执行子任务，<br>
+ * 需要完成200万个具有父子关系的任务时，也需要200万个线程，显然这是不可行的。<br>
+ * ForkJoinPool是使用ForkJoin的分治法解决有父子关系的多线程任务的线程池？<br>
+ * 之前我的爬虫项目，也碰到了父子关系的多线程任务，当时我把按优先级执行任务的逻辑“托管”给PriorityBlockingQueue<br>
+ * 或许ForkJoinPool有着更为高效的解决方案 TODO
+ * PriorityBlockingQueue的排序逻辑也需要了解TODO
+ */
+class ForkJoinPoolTest {
+
+    @Test
+    void go() {
+
+        ForkJoinPool pool;
+        ForkJoinTask task;
+    }
 }
