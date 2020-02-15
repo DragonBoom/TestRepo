71a7a84aaeb0cafae2c8786327dc223e7e95de8d
DragonBoom <1312449403@qq.com>
2020-02-15 11:14:12 +0800

    2020.02.15 by home 2

--- src/main/java/indi/spring/jpa/TestEntityDao2.java
+++ src/main/java/indi/spring/jpa/TestEntityDao2.java
@@ -2,6 +2,6 @@ package indi.spring.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TestEntityDao2 extends JpaRepository<TestEntityDO2, Long> {
+public interface TestEntityDao2 extends JpaRepository<TestEntityDO2, Long>, IndividualQueryDao {
 
 }
