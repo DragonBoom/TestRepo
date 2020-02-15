b1797feadbcc30a1adc9d2d2e0d1c2b9f67dfdfc
DragonBoom <1312449403@qq.com>
2020-02-15 11:12:47 +0800

    2020.02.15 by home 11

---
+++ src/main/java/indi/spring/jpa/IndividualQueryDaoImpl.java
@@ -0,0 +1,14 @@
+package indi.spring.jpa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IndividualQueryDaoImpl implements IndividualQueryDao {
+
+    @Override
+    public void go() {
+        // TODO Auto-generated method stub
+        System.out.println("go!!!");
+    }
+
+}
